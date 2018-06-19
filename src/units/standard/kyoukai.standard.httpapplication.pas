945fd046ffeb19acf405fa87e6fa2713c0090eeb
Ryuuji 高橋 隆司 <dioaffriza@gmail.com>
2018-06-20 00:36:27 +0800

    Now, console displays the port number correctly

--- src/units/standard/kyoukai.standard.httpapplication.pas
+++ src/units/standard/kyoukai.standard.httpapplication.pas
@@ -23,8 +23,28 @@ uses
   Kyoukai.Standard.WebRouter;
 
 type
-  TKyoukaiApp = class(TKyCustHTTPServer)
 
+  TKyoukaiApp = class(TCustomApplication)
+  private
+    fServer: TKyCustHTTPServer;
+    function ReadRouter: TKyRoutes;
+    procedure WriteRouter(ARouter: TKyRoutes);
+    function ReadFileRoutes: TFileRouteMap;
+    procedure WriteFileRoutes(AFileRoute: TFileRouteMap);
+    function ReadIsActive: boolean;
+    procedure WriteToActive(AState: boolean);
+    function ReadPort: word;
+    procedure WritePort(APort: word);
+    function ReadThreaded: boolean;
+    procedure WriteThreaded(IsThreaded: boolean);
+  public
+    property Threaded: boolean read ReadThreaded write WriteThreaded;
+    property Port: word read ReadPort write WritePort;
+    property Router: TKyRoutes read ReadRouter write WriteRouter;
+    property FileRoutes: TFileRouteMap read ReadFileRoutes write WriteFileRoutes;
+    property Active: boolean read ReadIsActive write WriteToActive;
+    constructor Create(AOwner: TComponent); override;
+    destructor Destroy; override;
   end;
 
 var
@@ -32,11 +52,73 @@ var
 
 implementation
 
+function TKyoukaiApp.ReadThreaded: boolean;
+begin
+  Result := fServer.Threaded;
+end;
+
+procedure TKyoukaiApp.WriteThreaded(IsThreaded: boolean);
+begin
+  fServer.Threaded := IsThreaded;
+end;
+
+function TKyoukaiApp.ReadRouter: TKyRoutes;
+begin
+  Result := fServer.Router;
+end;
+
+procedure TKyoukaiApp.WriteRouter(ARouter: TKyRoutes);
+begin
+  fServer.Router := ARouter;
+end;
+
+function TKyoukaiApp.ReadFileRoutes: TFileRouteMap;
+begin
+  Result := fServer.FileRoutes;
+end;
+
+procedure TKyoukaiApp.WriteFileRoutes(AFileRoute: TFileRouteMap);
+begin
+  fServer.FileRoutes := AFileRoute;
+end;
+
+function TKyoukaiApp.ReadIsActive: boolean;
+begin
+  Result := fServer.Active;
+end;
+
+function TKyoukaiApp.ReadPort: word;
+begin
+  Result := fServer.Port;
+end;
+
+procedure TKyoukaiApp.WritePort(APort: word);
+begin
+  fServer.Port := APort;
+end;
+
+procedure TKyoukaiApp.WriteToActive(AState: boolean);
+begin
+  WriteLn('Registered into port: ', KyoukaiApp.Port);
+  fServer.Active := AState;
+end;
+
+constructor TKyoukaiApp.Create(AOwner: TComponent);
+begin
+  inherited Create(AOwner);
+  fServer := TKyCustHTTPServer.Create(Self);
+end;
+
+destructor TKyoukaiApp.Destroy;
+begin
+  FreeAndNil(fServer);
+  inherited Destroy;
+end;
+
 initialization
   Routes := TKyRoutes.Create;
   KyoukaiApp := TKyoukaiApp.Create(nil);
   KyoukaiApp.Router := Routes;
-  WriteLn('Registered into port: ', KyoukaiApp.Port);
 
 finalization
   FreeAndNil(Routes);
